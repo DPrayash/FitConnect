d13bd956bc5cdb59bb12bebee0f4d60e99c354db
ubuntu <ubuntu@ip-172-31-9-187.ap-south-1.compute.internal>
2023-09-19 06:47:50 +0000

    Added Authentication-Service

--- Authentication-service/src/main/java/com/stackroute/controller/JwtController.java
+++ Authentication-service/src/main/java/com/stackroute/controller/JwtController.java
@@ -2,6 +2,7 @@ package com.stackroute.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,8 @@ public class JwtController {
 
     @Autowired
     private JwtService jwtService;
+    
+ 
 
     @PostMapping({"/authenticate"})
     public JwtResponse createJwtToken(@RequestBody JwtRequest jwtRequest) throws Exception {
