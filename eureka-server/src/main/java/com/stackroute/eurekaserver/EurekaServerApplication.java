0f0937c4a306ea4457f0ddc84ad9953eca10a7ae
ubuntu <ubuntu@ip-172-31-9-187.ap-south-1.compute.internal>
2023-09-19 06:52:35 +0000

    update eureka-service

--- eureka-server/src/main/java/com/stackroute/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/stackroute/eurekaserver/EurekaServerApplication.java
@@ -2,8 +2,11 @@ package com.stackroute.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
