e4c34d665f5411330136e59fe5c52ccc4309b9b7
Ubuntu <ubuntu@ip-172-31-36-181.ap-south-1.compute.internal>
2023-09-21 05:23:38 +0000

    updated

--- product-webapp-service/webapp/src/app/app.module.ts
+++ product-webapp-service/webapp/src/app/app.module.ts
@@ -4,10 +4,10 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { FormsModule } from '@angular/forms';
+
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -18,7 +18,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-
+import { MatDialogModule } from '@angular/material/dialog';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -35,7 +35,7 @@ import { PlansComponent } from './components/plans/plans.component';
 import { EquipmentsComponent } from './components/equipments/equipments.component';
 import { TrainersComponent } from './components/trainers/trainers.component';
 import { GalleryComponent } from './components/gallery/gallery.component';
-
+import { FormsModule } from '@angular/forms';
 import { UserHomeComponent } from './components/user-home/user-home.component';
 import { MembershipComponent } from './components/membership/membership.component';
 import { LoadingComponent } from './components/loading/loading.component';
@@ -44,6 +44,12 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { UpdateProfileComponent } from './components/update-profile/update-profile.component';
 import { AdminchatsectionComponent } from './components/adminchatsection/adminchatsection.component';
 import { UserchatsectionComponent } from './components/userchatsection/userchatsection.component';
+import { AuthGuard } from './_auth/auth.guard';
+import { AuthInterceptor } from './_auth/auth.interceptor';
+import { AuthenticationService } from './services/authentication.service';
+import { BookedComponent } from './components/booked/booked.component';
+import { BookComponent } from './components/book/book.component';
+import { DialogComponent } from './components/dialog/dialog.component';
 
 @NgModule({
   declarations: [
@@ -66,6 +72,9 @@ import { UserchatsectionComponent } from './components/userchatsection/userchats
     UpdateProfileComponent,
     AdminchatsectionComponent,
     UserchatsectionComponent,
+    BookedComponent,
+    BookComponent,
+    DialogComponent,
   ],
   imports: [
     BrowserModule,
@@ -89,9 +98,18 @@ import { UserchatsectionComponent } from './components/userchatsection/userchats
     MatDatepickerModule,
     MatNativeDateModule,
     MatProgressSpinnerModule,
-    MatGridListModule
+    MatGridListModule,
+    MatDialogModule
   ],
-  providers: [],
+  providers: [
+    AuthGuard,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass:AuthInterceptor,
+      multi:true
+    },
+    AuthenticationService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
